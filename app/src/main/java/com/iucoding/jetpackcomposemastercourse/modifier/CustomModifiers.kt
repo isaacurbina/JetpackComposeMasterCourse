1da8ac535429fd5c3e322ce9152cbad9577e78eb
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-01-09 16:16:15 -0600

    12 - Simple Custom Modifiers (#5)

---
+++ app/src/main/java/com/iucoding/jetpackcomposemastercourse/modifier/CustomModifiers.kt
@@ -0,0 +1,40 @@
+package com.iucoding.jetpackcomposemastercourse.modifier
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+fun Modifier.applyIf(
+	condition: Boolean,
+	modifier: Modifier.() -> Modifier
+): Modifier {
+	return if (condition) {
+		this.then(modifier())
+	} else this
+}
+
+@Composable
+fun SimpleModifierDemo(
+	modifier: Modifier = Modifier
+) {
+	Box(
+		modifier = Modifier
+			.size(100.dp)
+			.applyIf(true) {
+				background(Color.Red)
+					.padding(16.dp)
+			}
+	)
+}
+
+@Preview
+@Composable
+private fun SimpleModifierDemoPreview() {
+	SimpleModifierDemo()
+}
