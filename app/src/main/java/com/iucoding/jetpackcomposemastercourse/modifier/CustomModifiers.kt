af6bd9718b2ba51c195b3df5f2dcb472500af9f5
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-01-10 17:16:00 -0600

    13 - Composable Custom Modifiers (#6)

--- app/src/main/java/com/iucoding/jetpackcomposemastercourse/modifier/CustomModifiers.kt
+++ app/src/main/java/com/iucoding/jetpackcomposemastercourse/modifier/CustomModifiers.kt
@@ -7,7 +7,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 fun Modifier.applyIf(
@@ -19,17 +22,49 @@ fun Modifier.applyIf(
 	} else this
 }
 
+fun Modifier.printConstraints(tag: String): Modifier {
+	return layout { measurable, constraints ->
+		println("$tag: $constraints")
+		val placeable = measurable.measure(
+			constraints = constraints
+		)
+
+		layout(placeable.width, placeable.height) {
+			placeable.place(0, 0)
+		}
+	}
+}
+
+@Composable
+fun Modifier.negativePadding(horizontal: Dp): Modifier {
+	val density = LocalDensity.current
+	val px = with(density) {
+		horizontal.roundToPx()
+	}
+	return layout { measurable, constraints ->
+		val placeable = measurable.measure(
+			constraints = constraints.copy(
+				minWidth = constraints.minWidth + 2 * px,
+				maxWidth = constraints.maxWidth + 2 * px
+			)
+		)
+		layout(placeable.width, placeable.height) {
+			placeable.place(0, 0)
+		}
+	}
+}
+
 @Composable
 fun SimpleModifierDemo(
 	modifier: Modifier = Modifier
 ) {
 	Box(
-		modifier = Modifier
-			.size(100.dp)
-			.applyIf(true) {
-				background(Color.Red)
-					.padding(16.dp)
-			}
+		modifier = modifier
+            .size(100.dp)
+            .applyIf(true) {
+                background(Color.Red)
+                    .padding(16.dp)
+            }
 	)
 }
 
